2cfbd44db57fc6a1ad77626e764a2c8358716d83
Gelita 04 <gelacornelio3@gmail.com>
2022-12-26 23:12:19 -0500

    my first commit ,yupiiiii

---
+++ cp1/cp1.cs
@@ -0,0 +1,18 @@
+using System; 
+/*int[] a = { -2,0,6,-10,2};
+int m = int.MinValue;
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] > m)
+    {
+        m = a[i];
+    }
+}
+
+System.Console.WriteLine(m);*/
+Console.WriteLine("Escriba la longitud de un lado del triangulo");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Escribe la longitud de otro lado");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Escriba la longitud del lado que falta");
+int c = int.Parse(Console.ReadLine());
